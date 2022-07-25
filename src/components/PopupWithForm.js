cdaf4c5b446f5feb8da434af51f96caca5928c81
Vladislav <obivaniche@yandex.ru>
2022-07-26 00:06:41 +0300

    Update PopupWithForm.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,14 +3,14 @@ import React from 'react';
 function PopupWithForm({name, title, children, buttonTitle, isOpen}) {
 
     return (
-        <div className={`popup popup-${name}` + (isOpen && 'popup_opened')}>
+        <div className={`popup popup_${name} ${isOpen && "popup_opened"}`}>
             <div className="popup__container">
-                <button className="button popup__close-button close-name" type="button"></button>
+                <button className={`button popup__close-button close-${name}`} type="button"></button>
                 <h2 className="popup__title">{title}</h2>
-                <form className="form form-edit" name="edit" noValidate>
+                <form className={`form form-${name}`} name={name} noValidate>
                     <fieldset className="form__set">
                         {children}
-                        <button className="button form__submit-button submit-edit" type="submit">{buttonTitle}</button>
+                        <button className={`button form__submit-button submit-${name}`} type="submit">{buttonTitle}</button>
                     </fieldset>
                 </form>
             </div>
